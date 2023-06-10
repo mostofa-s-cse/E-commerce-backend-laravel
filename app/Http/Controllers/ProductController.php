c6daf2c9e304d1e99e70688e7d2c663244a9f75e
mostofa-s-cse <mostofa.s.cse@gmail.com>
2023-06-10 22:28:05 +0600

    product search api done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,16 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+
 class ProductController extends Controller
 {
     //
     function addProduct(Request $req)
     {
         $product = new Product;
-        $product->name=$req->input('name');
-        $product->price=$req->input('price');
-        $product->description=$req->input('description');
-        $product->image=$req->file('image')->store('products');
+        $product->name = $req->input('name');
+        $product->price = $req->input('price');
+        $product->description = $req->input('description');
+        $product->image = $req->file('image')->store('products');
         $product->save();
         return $product;
     }
@@ -23,17 +24,33 @@ class ProductController extends Controller
     }
     function delete($id)
     {
-        $result = Product::where('id',$id)->delete();
-        if($result)
-        {
-            return["result"=>"Product has been delete"];
-        }
-        else{
-            return ["result"=>"Operation faild"];
+        $result = Product::where('id', $id)->delete();
+        if ($result) {
+            return ["result" => "Product has been delete"];
+        } else {
+            return ["result" => "Operation faild"];
         }
     }
-    function getSingleProduct($id)
+    function getProduct($id)
     {
         return Product::find($id);
     }
+
+    function updateProduct($id, Request $req)
+    {
+        $product = Product::find($id);
+        $product->name = $req->input('name');
+        $product->price = $req->input('price');
+        $product->description = $req->input('description');
+        if ($req->file('file')) {
+            $product->image = $req->file('file')->store('products');
+        }
+        $product->save();
+        return $product;
+    }
+    function search($key)
+    {
+        return Product::where('name','Like',"%$key%")->get();
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,8 @@ Route::post('login',[UserController::class,'login']);
 Route::post('addproduct',[ProductController::class,'addProduct']);
 Route::get('list',[ProductController::class,'list']);
 Route::delete('delete/{id}',[ProductController::class,'delete']);
-Route::get('singleproduct/{id}',[ProductController::class,'getSingleProduct']);
+Route::get('product/{id}',[ProductController::class,'getProduct']);
+Route::put('updateproduct/{id}',[ProductController::class,'updateProduct']);
 
+Route::get('search/{key}',[ProductController::class,'search']);
 
