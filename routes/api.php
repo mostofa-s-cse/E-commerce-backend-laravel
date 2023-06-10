c13ade33ad81f318a56612e6065c8c41bb070be8
mostofa-s-cse <mostofa.s.cse@gmail.com>
2023-06-10 16:19:57 +0600

    add product done

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::post('signup',[UserController::class,'signup']);
 Route::post('login',[UserController::class,'login']);
+Route::post('addproduct',[ProductController::class,'addProduct']);
